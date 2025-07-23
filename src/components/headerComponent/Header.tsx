efc82fea42f491118bf93d9ed7d580510da3d0f7
Alejo <alejogustavof@gmail.com>
2025-07-23 07:17:46 -0300

    refactor header and add logo

--- src/components/headerComponent/Header.tsx
+++ src/components/headerComponent/Header.tsx
@@ -2,9 +2,12 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className="bg-blue-700 text-white py-4 shadow-md">
+    <header className="bg-[#1a337d] text-white py-4 shadow-md">
       <div className="container mx-auto flex items-center justify-between px-4">
-        <h1 className="text-2xl font-bold tracking-tight">Aid Distribution Dashboard</h1>
+        <div className="flex items-center">
+          <img src="/logo.svg" alt="logo" className="w-10 h-10" />
+          <h1 className="hidden sm:block text-xl font-regular tracking-tight ml-3">Aid Distribution Dashboard</h1>
+        </div>
         <nav>
           <ul className="flex space-x-6">
             <li>
