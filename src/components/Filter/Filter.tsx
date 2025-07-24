0b5af94450125f5f128c5f1f3a5c54f333e834ab
Alejo <alejogustavof@gmail.com>
2025-07-24 07:07:05 -0300

    delete react imports

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 
 export interface FilterProps {
   label: string;
--- src/components/Table/TableContainer.tsx
+++ src/components/Table/TableContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState, useEffect } from 'react';
+import { useMemo, useState, useEffect } from 'react';
 import { distributions } from '../../service/MockApi';
 import type { Distribution } from '../../models/Beneficiary';
 import { Table } from './Table';
